4baf5372baa8ef3ed80ad0e0dd19ed34f4260230
mundayeon <mundayeon93@gmail.com>
2023-01-24 18:52:24 +0900

    Add: 초기셋팅

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+import CATEGORY_DATA from './CategoryData';
+
+const Nav = () => {
+  return <div>Nav</div>;
+};
+
+export default Nav;
